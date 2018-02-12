4b9c572fa82e3e26d74297ac4a7bfae72c82d2e5
bmodie <32982721+bmodie@users.noreply.github.com>
2018-02-11 19:55:07 -0600

    save

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -3,26 +3,25 @@ import csv
 import pandas as pd 
 import dateutil
 
-
 # Path to collect data
 budget_data_1_CSV = os.path.join('budget_data_1.csv')
 budget_data_2_CSV = os.path.join('budget_data_2.csv')
 
-# df = pd.read_csv(budget_data_1_CSV, names=['Date', 'Revenue'])
-# df1 = pd.read_csv(budget_data_2_CSV, header=None)
+# convert to datafile
+df = pd.read_csv(budget_data_1_CSV, names=['Date', 'Revenue'])
+df1 = pd.read_csv(budget_data_2_CSV, header=None)
 
-# all_budget_data = pd.concat([df, df1], axis=0)
+# concatenate the two files
+all_budget_data = pd.concat([df, df1], axis=0)
 
-numbers1 = budget_data_1_CSV[1]
-numbers2 = budget_data_2_CSV[2]
-# Define the function and have it accept the 'BD1' as its sole parameter
-#def data_insights(revenue):
-    
-    # total # of months in dataset
-#total_months = str(all_budget_data[0])
+# Define the function
+def revenue(all_budget_data[1]):
 
     # total revenue gained
-total_rev = sum(float(numbers1) + float(numbers2))
+    total_rev = sum(float(numbers1) + float(numbers2))
+
+    # total # of months in dataset
+    #total_months = str(all_budget_data[0])
 
     # average change in revenue between months
     #avg_rev = 
